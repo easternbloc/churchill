f0bade507b32748c51fbd0df2da0e47f81620093
Ben Marvell <madmarv187@gmail.com>
2013-10-04 11:13:41 +0100

    Refactored so you can add multiple loggers to the middleware.
    for example:
      app.use(require('churchill').add(logger, 'express').add(logger, 'somethingelse'));
    
    you can also use it like so:
      app.use(require('churchill')(logger, 'express'));
    
    Bumped version number for npm release.

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-var url = require('url');
+var url = require('url'),
+    add = function (logger, level) {
+        loggers.push([logger, level]);
+        return fn;
+    },
+    loggers = [],
+    fn,
+    Churchill;
 
-module.exports = function (logger, level) {
-
-    return function (req, res, next) {
-        var requestEnd = res.end,
-            requestToLog = {
-                status: null,
-                method: req.method,
-                url: req.originalUrl
-            };
-
-        //if we're using a connect middleware logger this will already exist
-        req._startTime = req._startTime || new Date();
-
-        // Use the end function
-        res.end = function (chunk, encoding) {
-            res.end = requestEnd;
-            res.end(chunk, encoding);
-
-            requestToLog.status = res.statusCode;
-            requestToLog.response_time = (new Date() - req._startTime) + 'ms';
-
-            level = level || 'info';
-
-            logger.log(level, requestToLog);
+fn = function (req, res, next) {
+    var requestEnd = res.end,
+        requestToLog = {
+            status: null,
+            method: req.method,
+            url: req.originalUrl
         };
 
-        next();
+    //if we're using a connect middleware logger this will already exist
+    req._startTime = req._startTime || new Date();
+
+    // Use the end function
+    res.end = function (chunk, encoding) {
+        res.end = requestEnd;
+        res.end(chunk, encoding);
+
+        requestToLog.status = res.statusCode;
+        requestToLog.response_time = (new Date() - req._startTime) + 'ms';
+
+        loggers.forEach(function (arr) {
+            var logger = arr[0],
+                level = arr[1];
+            level = level || 'info';
+            logger.log(level, requestToLog);
+        });
     };
+
+    next();
 };
+
+fn.add = add;
+
+Churchill = function (logger, level) {
+    add(logger, level);
+    return fn;
+};
+
+Churchill.add = add;
+
+module.exports = Churchill;
